e430d9987a37dde53ea8357bbad99ac6c286ce56
Abdul Khan <akhan17@outlook.com>
2020-04-15 22:08:22 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -5,7 +5,7 @@ class ChattyStudent
   end
 
   def hello
-    "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm ok
+    puts "Hey there! I'm so excited to learn stuff.\nHow are you doing today? I'm ok
 ay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any s
 poilers? Okay well let me just tell you who died...\n"
   end
